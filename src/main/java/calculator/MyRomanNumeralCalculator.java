45b47bca3f7d3b153b0ba41d89946bff64aeeca1
dale <dtlim@voyagerinnovation.com>
2016-01-22 14:36:43 +0800

    miggy

--- src/main/java/calculator/MyRomanNumeralCalculator.java
+++ src/main/java/calculator/MyRomanNumeralCalculator.java
@@ -4,10 +4,11 @@ public class MyRomanNumeralCalculator implements RomanNumeralCalculator {
     String[] millionString = {"M"};
     String[] hundredThousandsString = {"C", "CC", "CCC", "D", "CD", "DC", "DCC", "DCCC", "CM"};
     String[] tenThousandsString = {"X", "XX", "XXX", "L", "XL", "LX", "LXX", "LXXX", "XC"};
-    String[] thousandsString = {"m", "mm", "mmm", "V", "mV", "Vm", "Vmm", "Vmmm", "mX", "X"};
+    String[] thousandsString = {"m", "mm", "mmm", "V", "mV", "Vm", "Vmm", "Vmmm", "mX"};
     String[] hundredsString = {"c", "cc", "ccc", "d", "cd", "dc", "dcc", "dccc", "cm"};
     String[] tensString = {"x", "xx", "xxx", "l", "xl", "lx", "lxx", "lxxx", "xc"};
     String[] onesString = {"i", "ii", "iii", "v", "iv", "vi", "vii", "viii", "ix"};
+    String[][] romanNumeralString = {onesString, tensString, hundredsString, thousandsString, tenThousandsString, hundredThousandsString, millionString};
     int[] numbers = {1, 2, 3, 5, 4, 6, 7, 8, 9, 10};
 
     @Override
@@ -29,119 +30,56 @@ public class MyRomanNumeralCalculator implements RomanNumeralCalculator {
     }
 
     public int romanNumeralToNumber(String romanNumeral) {
-        int million = 0;
-        int hundredThousands = 0;
-        int tenThousands = 0;
-        int thousands = 0;
-        int hundreds = 0;
-        int tens = 0;
-        int ones = 0;
         int sign = 1;
+        int sum = 0;
+        int[] digits = new int[7];
 
         if (romanNumeral.contains("-")) {
             sign = -1;
         }
 
-        for (int a = 0; a < millionString.length; a++) {
-            if (romanNumeral.contains(millionString[a])) {
-                million = numbers[a];
-            }
-        }
-        for (int a = 0; a < hundredThousandsString.length; a++) {
-            if (romanNumeral.contains(hundredThousandsString[a])) {
-                hundredThousands = numbers[a];
-            }
-        }
-        for (int a = 0; a < tenThousandsString.length; a++) {
-            if (romanNumeral.contains(tenThousandsString[a])) {
-                tenThousands = numbers[a];
-            }
-        }
-        for (int a = 0; a < thousandsString.length; a++) {
-            if (romanNumeral.contains(thousandsString[a])) {
-                thousands = numbers[a];
-            }
-        }
-        for (int a = 0; a < hundredsString.length; a++) {
-            if (romanNumeral.contains(hundredsString[a])) {
-                hundreds = numbers[a];
-            }
-        }
-        for (int a = 0; a < tensString.length; a++) {
-            if (romanNumeral.contains(tensString[a])) {
-                tens = numbers[a];
-            }
-        }
-        for (int a = 0; a < onesString.length; a++) {
-            if (romanNumeral.contains(onesString[a])) {
-                ones = numbers[a];
+        for (int a = 0; a < romanNumeralString.length; a++) {
+            for (int x = 0; x < romanNumeralString[a].length; x++) {
+                if (romanNumeral.contains(romanNumeralString[a][x])) {
+                    if (x == 0 && a !=0 && a != romanNumeralString.length) {
+                        if (romanNumeral.contains(romanNumeralString[a--][8])) {
+                            continue;
+                        }
+                    }
+                    digits[a] = numbers[x];
+                }
             }
         }
 
-        return ( (million * 1000000) + (hundredThousands * 100000) + (tenThousands * 10000) +
-                (thousands * 1000) + (hundreds * 100) + (tens * 10) + ones) * sign;
+        for (int a = 0; a < digits.length; a++) {
+            sum += digits[a] * Math.pow(10, a);
+        }
+        return sum;
     }
 
     public String numberToRomanNumeral(int number) {
         String sign = "";
-        int million = 0;
-        int hundredThousands = 0;
-        int tenThousands = 0;
-        int thousands = 0;
-        int hundreds = 0;
-        int tens = 0;
-        int ones = 0;
+        int[] digits = new int[7];
+        String[] RN = new String[7];
 
         if (number < 0) {
             sign = "-";
             number *= -1;
         }
 
-        million = number / 1000000;
-        number %= 1000000;
-        hundredThousands = number / 100000;
-        number %= 100000;
-        tenThousands = number / 10000;
-        number %= 10000;
-        thousands = number / 1000;
-        number %= 1000;
-        hundreds = number / 100;
-        number %= 100;
-        tens = number / 10;
-        number %= 10;
-        ones = number;
-
-        String millionRN = "";
-        String hundredThousandsRN = "";
-        String tenThousandsRN = "";
-        String thousandsRN = "";
-        String hundredsRN = "";
-        String tensRN = "";
-        String onesRN = "";
-
-        if(million != 0) {
-            millionRN = millionString[getIndex(million)];
-        }
-        if(hundredThousands != 0) {
-            hundredThousandsRN = hundredThousandsString[getIndex(hundredThousands)];
-        }
-        if(tenThousands != 0) {
-            tenThousandsRN = tenThousandsString[getIndex(tenThousands)];
-        }
-        if (thousands != 0) {
-            thousandsRN = thousandsString[getIndex(thousands)];
-        }
-        if (hundreds != 0) {
-            hundredsRN = hundredsString[getIndex(hundreds)];
-        }
-        if (tens != 0) {
-            tensRN = tensString[getIndex(tens)];
-        }
-        if (ones != 0) {
-            onesRN = onesString[getIndex(ones)];
+        for (int a = digits.length - 1; a >= 0; a--) {
+            digits[a] = (int)(number / Math.pow(10, a));
+            number %= Math.pow(10, a);
         }
 
-        return (sign + millionRN + hundredThousandsRN + tenThousandsRN + thousandsRN + hundredsRN + tensRN + onesRN);
+        String endNumber = "";
+        for (int a = RN.length - 1; a >= 0; a--) {
+            if (digits[a] != 0) {
+                endNumber += romanNumeralString[a][getIndex(digits[a])];
+            }
+        }
+
+        return endNumber;
     }
 
     public int getIndex(int x) {
